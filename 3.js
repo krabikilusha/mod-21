1e5fd5f81e78b925aa3620babe41877e90866177
krabikilusha <pavlov.trezv.sind@gmail.com>
2023-03-12 12:27:45 +0300

    new mod

---
+++ 3.js
@@ -0,0 +1,31 @@
+//Задание 3.
+// Получение данных в localStorage
+let myKey = localStorage.getItem('tstKey');
+
+// Если localStorage пуст
+if (myKey === null) {
+	var usrName = prompt(`Добро пожаловать! Назовите, пожалуйста, ваше имя`);
+	var curDate = new Date();
+
+	//JSON для записи
+	const jsonString = `
+  {
+    "name": ${usrName},
+    "date": ${curDate}
+  }`;
+
+	//Запись в localStorage
+	localStorage.setItem('tstKey', jsonString);
+} else {
+	alert(`Добрый день, ${myKey.name}! Давно не виделись. В последний раз вы были у нас ${myKey.date}`);
+
+	//JSON для перезаписи
+	const jsonString = `
+  {
+    "name": ${myKey.name},
+    "date": ${new Date()}
+  }`;
+
+	//Перезапись localStorage
+	localStorage.setItem('tstKey', jsonString);
+}
